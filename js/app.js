28a01bb4a72865ba7ef3ff3309babb1e392e4773
ashensavi <ashensavinda0@gmail.com>
2024-10-29 23:56:51 +0530

    ff

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
 
 // }
 
-let numbers = [10,20,30,40,50,60,70,80,90,100];
+// let numbers = [10,20,30,40,50,60,70,80,90,100];
 
 // let sum = 0;
 
@@ -53,61 +53,22 @@ let numbers = [10,20,30,40,50,60,70,80,90,100];
 // console.log(isTrue);//false because === checks for datatype also
 
 
+function handleClick(value){
 
-
-
-function calc(){
-    let number01 = document.getElementById("txtNumber01").value;
-    let number02 = document.getElementById("txtNumber02").value;
-    let operator = document.getElementById("operator").value;
-    let lblOutput = document.getElementById("lblOutput"); 
-
-function getVal(){
-    let val1 = document.getElementById("1").value;
-    let val2 = document.getElementById("2").value;
-    let val3 = document.getElementById("3").value;
-    let val4 = document.getElementById("4").value;
-    let val5 = document.getElementById("5").value;
-    let val6 = document.getElementById("6").value;
-    let val7 = document.getElementById("7").value;
-    let val8 = document.getElementById("8").value;
-    let val9 = document.getElementById("9").value;
-    let val0 = document.getElementById("0").value;
-}
-    
-
-   let Output=0;
-    switch(operator){
-        case "+":
-            let sum;
-            sum=parseInt(number01)+parseInt(number02);
-            Output=sum;
-            break;
-        case "-":
-            Output=number01-number02;
-            break;
-        case "*":
-            Output=number01*number02;
-            break;
-        case "/":
-            Output=number01/number02;
-            break;
-
+    console.log(value);
+    let displayValue = document.getElementById("displayValue");
+    if (displayValue.innerHTML === "0") {
+        displayValue.innerHTML = value;
+    } else {
+        displayValue.innerHTML += value; 
     }
-     
-
-    console.log(number01+""+operator+""+number02+"="+Output);
-  lblOutput.innerHTML=Output;
 }
 
+function calc(value){
+     
+    if(value=="+" || value == "-" || value == "*" || value == "/"){
 
+        let firstNum = 
+    }
 
-
-
-// function calc(){
-//     let displayText = document.getElementById("displayText").value;
-//     let lblOutput = document.getElementById("lblOutput");
-//     let sum = eval(displayText);
-//     lblOutput.innerHTML=sum;
-//     alert(sum);
-// }
+}
